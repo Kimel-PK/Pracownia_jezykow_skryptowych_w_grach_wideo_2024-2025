9bc5b882223adc96fdea6dee9746c6bbf3869f41
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2025-05-10 22:10:03 +0200

    Mario - lives

--- ruby/main.rb
+++ ruby/main.rb
@@ -15,16 +15,16 @@ LEVEL_MAP = [
     "",
     "",
     "",
-    "                      ?       ",
+    "                      ?                                                         XXXXXXXXX   XXX?",
     "",
     "",
     "",
-    "                ?   X?X?X       ",
-    "                                      ╒╕",
-    "                            ╒╕        ├┤",
-    "                            ├┤        ├┤      ├┤",
-    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-    "################################################"
+    "                ?   X?X?X                     ╒╕         ╒╕                  X?X               ?",
+    "                                      ╒╕      ├┤         ├┤",
+    "                            ╒╕        ├┤      ├┤         ├┤",
+    "                            ├┤        ├┤      ├┤         ├┤",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  OOOOOOOOOOOOOOOO   OOOOOO",
+    "#####################################################################  ################   ######"
 ]
 
 tileset = Tileset.new(
@@ -38,6 +38,7 @@ tileset = Tileset.new(
 tileset.define_tile('grass_top', 0, 0)
 tileset.define_tile('dirt', 2, 6)
 tileset.define_tile('brick', 2, 9)
+tileset.define_tile('question_mark_block', 3, 9)
 tileset.define_tile('pipe_top_left', 4, 6)
 tileset.define_tile('pipe_top_right', 5, 6)
 tileset.define_tile('pipe_left', 4, 7)
@@ -47,7 +48,7 @@ tileset.define_tile('undefined', 4, 9)
 # Player
 player = Rectangle.new(
     x: TILE_SIZE * 2,
-    y: TILE_SIZE * 6,
+    y: TILE_SIZE * 11,
     width: TILE_SIZE,
     height: TILE_SIZE * 2,
     color: 'blue'
@@ -55,42 +56,51 @@ player = Rectangle.new(
 
 player_vy = 0
 on_ground = false
+died = false
 
-# Create level tiles
 blocks = []
 tiles = Hash.new { |hash, key| hash[key] = [] }
 
-LEVEL_MAP.each_with_index do |row, y|
-    row.chars.each_with_index do |char, x|
-        if char == " "
-            next
-        end
-        
-        # Collision box
-        blocks << Square.new(
-            x: x * TILE_SIZE,
-            y: y * TILE_SIZE,
-            size: TILE_SIZE,
-            color: [1, 0, 0, 0]
-        )
-        
-        # Graphics tile
-        if char == "#"
-            tiles["dirt"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "O"
-            tiles["grass_top"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "X"
-            tiles["brick"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "╒"
-            tiles["pipe_top_left"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "╕"
-            tiles["pipe_top_right"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "├"
-            tiles["pipe_left"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        elsif char == "┤"
-            tiles["pipe_right"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
-        else
-            tiles["undefined"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+def create_level(blocks, tiles)
+    # Clear arrays
+    blocks.clear
+    tiles.clear
+    
+    # Create level tiles
+    LEVEL_MAP.each_with_index do |row, y|
+        row.chars.each_with_index do |char, x|
+            if char == " "
+                next
+            end
+            
+            # Collision box
+            blocks << Square.new(
+                x: x * TILE_SIZE,
+                y: y * TILE_SIZE,
+                size: TILE_SIZE,
+                color: [1, 0, 0, 0]
+            )
+            
+            # Graphics tile
+            if char == "#"
+                tiles["dirt"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "O"
+                tiles["grass_top"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "X"
+                tiles["brick"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "?"
+                tiles["question_mark_block"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "╒"
+                tiles["pipe_top_left"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "╕"
+                tiles["pipe_top_right"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "├"
+                tiles["pipe_left"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            elsif char == "┤"
+                tiles["pipe_right"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            else
+                tiles["undefined"] << { x: x * TILE_SIZE, y: y * TILE_SIZE }
+            end
         end
     end
 end
@@ -107,29 +117,41 @@ end
 
 on :key_up do |event|
     keys[event.key] = false
-    if event.key == "space" and player_vy < 0
+    if event.key == "space" and player_vy < 0 and !died
         player_vy /= 2
     end
 end
 
 def aabb_collision?(a, b)
-    if a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y
-        puts "collision"
-    end
     a.x < b.x + b.width &&
     a.x + a.width > b.x &&
     a.y < b.y + b.height &&
     a.y + a.height > b.y
 end
 
+# Main
+create_level(blocks, tiles)
+
+points = 0
+lives = 3
+
+hud = Text.new(
+    "Points: #{points}  Lives: #{lives}",
+    x: 10,
+    y: 10,
+    size: 20,
+    color: 'white',
+    z: 100  # draw above other objects
+)
+
 update do
-    
-    puts player.y
     
     # Movement
     dx = 0
-    dx -= MOVE_SPEED if keys['left']
-    dx += MOVE_SPEED if keys['right']
+    if !died
+        dx -= MOVE_SPEED if keys['left']
+        dx += MOVE_SPEED if keys['right']
+    end
     player.x += dx
 
     # Jump
@@ -141,19 +163,15 @@ update do
     # Apply gravity
     player_vy += GRAVITY
     player.y += player_vy
-    
-    puts "player vy #{player_vy}"
-    puts player.y
 
-    # Vertical collisions
+    # Check collisions
     on_ground = false
     blocks.each do |block|
-        if player.contains?(block.x + 1, block.y + 1) || block.contains?(player.x + 1, player.y + 1)
-            puts "player vertical collision"
+        # Vertical collisions
+        if aabb_collision?(player, block)
             # Collision from falling
             if player_vy > 0 && player.y + player.height > block.y
                 player.y = block.y - player.height
-                puts "snap player to vertical position"
                 player_vy = 0
                 on_ground = true
             end
@@ -164,12 +182,8 @@ update do
                 player_vy = 0
             end
         end
-    end
-    
-    puts "before horizontal collision #{player.y}"
-    
-    # Horizontal collisions
-    blocks.each do |block|
+        
+        # Horizontal collisions
         if aabb_collision?(player, block)
             if dx > 0
                 # Moving right — push player to the left edge of block
@@ -180,8 +194,6 @@ update do
             end
         end
     end
-    
-    puts "after horizontal collision #{player.y}"
 
     # Camera follow
     camera_x = player.x - Window.width / 2
@@ -206,7 +218,23 @@ update do
 
     player.x = 0 if player.x < 0
     player.x -= camera_x
-    puts "Next frame"
+    
+    if player.y > 500
+        if !died
+            player_vy = -15
+            died = true
+        else
+            # Lose life
+            lives -= 1
+            hud.text = "Points: #{points}  Lives: #{lives}"
+            
+            # Reset to start of level
+            create_level(blocks, tiles)
+            player.x = TILE_SIZE * 2
+            player.y = TILE_SIZE * 11
+            died = false
+        end
+    end
 end
 
 show
